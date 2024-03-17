0dd1f429cf26a6bd9d9de51136d11e3c5ac5584f
alonlevi <alon.levi3@mail.huji.ac.il>
2024-03-17 13:07:23 +0200

    Test

--- Geometry/include/ActsLUXEPipeline/LUXEGeometryConstraints.hpp
+++ Geometry/include/ActsLUXEPipeline/LUXEGeometryConstraints.hpp
@@ -66,6 +66,10 @@ namespace LUXEGeometry {
             Acts::AngleAxis3(-M_PI_2,
                 Acts::Vector3(1., 0., 0.)).toRotationMatrix();
 
+        const std::vector<std::pair<Acts::ActsScalar,Acts::ActsScalar>> MagneticFieldBounds =
+                {std::make_pair(-255_mm,255_mm),
+                 std::make_pair(-255_mm,255_mm),
+                 std::make_pair(-255_mm,255_mm)};
         /// Global rotation of the world volume
         /// in the Geant4 format for surfaces
         const CLHEP::HepRotationX g4WorldRotation =
