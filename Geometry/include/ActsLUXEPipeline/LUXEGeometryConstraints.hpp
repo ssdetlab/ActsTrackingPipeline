2837ad4719ed9888f95facf306ded3eadc451d94
ssdetlab <ssdetlab@gmail.com>
2024-04-02 21:02:46 +0300

    updating the constraints

--- Geometry/include/ActsLUXEPipeline/LUXEGeometryConstraints.hpp
+++ Geometry/include/ActsLUXEPipeline/LUXEGeometryConstraints.hpp
@@ -3,7 +3,8 @@
 #include "Acts/Definitions/Algebra.hpp"
 #include "Acts/Definitions/Units.hpp"
 
-#include "CLHEP/Vector/Rotation.h" 
+#include "G4Transform3D.hh" 
+#include "G4ThreeVector.hh"
 
 #include <numeric>
 #include <map>
@@ -19,6 +20,8 @@ namespace LUXEGeometry {
         const float chipSizeX = 29.94176_mm;
         const float chipSizeY = 13.76256_mm;
 
+        // Chip Id to the translation in the x direction
+        // for the inner stave
         const std::map<std::int32_t, Acts::ActsScalar> 
         chipXEven{ 
             {0, 67.73_mm}, 
@@ -32,6 +35,8 @@ namespace LUXEGeometry {
             {8, 308.53_mm}
         };
 
+        // Chip Id to the translation in the x direction
+        // for the outer stave
         const std::map<std::int32_t, Acts::ActsScalar> 
         chipXOdd{
             {0, 298.53_mm}, 
@@ -45,8 +50,9 @@ namespace LUXEGeometry {
             {8, 539.33_mm}
         };
 
+        // Stave Id to the translation in the z direction
         const std::map<std::int32_t, Acts::ActsScalar>
-        layerZ{
+        staveZ{
             {0, 3962.0125_mm},
             {1, 3950.0125_mm},
             {2, 4062.0125_mm},
@@ -57,84 +63,71 @@ namespace LUXEGeometry {
             {7, 4250.0125_mm}
         };
 
+        // All the staves are at the same y position
         Acts::ActsScalar chipY = 0.61872_mm;
 
-        const float chipTranslationY = chipY;
-        const std::map<int, float> chipTranslationXEven {
-            {0, chipXEven.at(0)},
-            {1, chipXEven.at(1)},
-            {2, chipXEven.at(2)},
-            {3, chipXEven.at(3)},
-            {4, chipXEven.at(4)},
-            {5, chipXEven.at(5)},
-            {6, chipXEven.at(6)},
-            {7, chipXEven.at(7)},
-            {8, chipXEven.at(8)}
-        };
-
-        const std::map<int, float> chipTranslationXOdd {
-            {0, chipXOdd.at(0)},
-            {1, chipXOdd.at(1)},
-            {2, chipXOdd.at(2)},
-            {3, chipXOdd.at(3)},
-            {4, chipXOdd.at(4)},
-            {5, chipXOdd.at(5)},
-            {6, chipXOdd.at(6)},
-            {7, chipXOdd.at(7)},
-            {8, chipXOdd.at(8)}
-        };
-
+        // Positions of the volumes
+        // encapsulating the layers
         const std::vector<Acts::ActsScalar> layerZPositions{
-            (layerZ.at(0) + layerZ.at(1))/2, 
-            (layerZ.at(2) + layerZ.at(3))/2,
-            (layerZ.at(4) + layerZ.at(5))/2,
-            (layerZ.at(6) + layerZ.at(7))/2
+            (staveZ.at(0) + staveZ.at(1))/2, 
+            (staveZ.at(2) + staveZ.at(3))/2,
+            (staveZ.at(4) + staveZ.at(5))/2,
+            (staveZ.at(6) + staveZ.at(7))/2
         };
 
-        const Acts::Vector3 dipoleTranslation{
-            0_mm, 0_mm, 2050_mm};
-
-        const Acts::Vector3 postironArmTranslation{
-            (chipXOdd.at(8) + chipXEven.at(0))/2, 0_mm, 2575_mm};
-
-        const Acts::Vector3 electronArmTranslation{
-            -(chipXOdd.at(8) + chipXEven.at(0))/2, 0_mm, 2575_mm};
-
+        // Tracker volume encapsulating the 
+        // whole detector
         const Acts::Vector3 trackerTranslation{
-            0_mm, 0_mm, 2575_mm};
-
-        /// Can be set to zero if there are 
-        /// no IN and OUT staves for a single layer
-        const Acts::ActsScalar deltaZ = 
-            (layerZ.at(0) - layerZ.at(1))/2 + 1_mm;
+            0_mm, 0_mm, 2150_mm};
 
         const std::vector<Acts::ActsScalar> trackerBounds = 
             {chipXOdd.at(8) + chipSizeX/2 + 1_mm, 
-                chipSizeY/2 + 1_mm, 1725_mm};
+                chipSizeY/2 + 1_mm, 2200_mm};
 
-        const std::vector<Acts::ActsScalar> armBounds = 
-            {(chipXOdd.at(8) - chipXEven.at(0))/2 + chipSizeX/2 + 1_mm, 
-                chipSizeY/2 + 1_mm, 1725_mm};
+        // Dipole volume encapsulating the
+        // magnetic field
+        const Acts::Vector3 dipoleTranslation{
+            0_mm, 0_mm, 2050_mm};
+
+        const std::vector<Acts::ActsScalar> dipoleBounds = 
+            {chipXOdd.at(8) + chipSizeX/2 + 1_mm, 
+                chipSizeY/2 + 1_mm, 600_mm};
+
+        const std::vector<Acts::ActsScalar> constantFieldDelta = 
+            {0_mm, 0_mm, 120_mm};
+
+        // Arm volume encapsulating the layers
+        const Acts::Vector3 armTranslation{
+            0_mm, 0_mm, (staveZ.at(7) + staveZ.at(0))/2};
+
+        // Layer volumes encapsulating the staves
+        const Acts::ActsScalar deltaZ = 
+            (staveZ.at(0) - staveZ.at(1))/2 + 1_mm;
 
         const std::vector<Acts::ActsScalar> layerBounds = 
-            {(chipXOdd.at(8) - chipXEven.at(0))/2 + chipSizeX/2 + 1_mm, 
+            {chipXOdd.at(8) + chipSizeX/2 + 1_mm, 
                 chipSizeY/2 + 1_mm, deltaZ};
-
-        const std::vector<std::pair<Acts::ActsScalar,Acts::ActsScalar>> MagneticFieldBounds =
-                {std::make_pair(-1000_mm,1000_mm),
-                 std::make_pair(1450_mm,2650_mm),
-                 std::make_pair(-100_mm,100_mm)};
 
         /// Global rotation of the world volume
         /// in the Acts format for volumes
-        const Acts::RotationMatrix3 actsWorldRotation = 
-            Acts::AngleAxis3(-M_PI_2,
+        const Acts::RotationMatrix3 actsToWorldRotation = 
+            Acts::AngleAxis3(M_PI_2,
                 Acts::Vector3(1., 0., 0.)).toRotationMatrix();
         
+        /// Global translation of the world volume
+        /// in the Acts format for volumes
+        const Acts::Translation3 actsToWorldTranslation = 
+            Acts::Translation3(0, 0, 0);
+        
+        /// In case we'll need something less trivial 
+        const Acts::Transform3 actsToWorld = 
+            Acts::Transform3(actsToWorldRotation * actsToWorldTranslation);
+
         /// Global rotation of the world volume
         /// in the Geant4 format for surfaces
-        const CLHEP::HepRotationX g4WorldRotation =
-            CLHEP::HepRotationX(-M_PI_2);
+        const G4Transform3D g4ToWorld = G4Transform3D(
+            CLHEP::HepRotationX(M_PI_2), 
+            G4ThreeVector(0, 0, 0));
     };
 
 } // namespace LUXEGeometry
