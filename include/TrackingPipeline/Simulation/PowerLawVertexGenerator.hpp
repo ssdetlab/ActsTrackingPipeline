50e81b862672e8a13a65d26870cdf43032a06909
ssdetlab <ssdetlab@gmail.com>
2025-03-13 16:07:18 +0200

    power law gen

--- include/TrackingPipeline/Simulation/PowerLawVertexGenerator.hpp
+++ include/TrackingPipeline/Simulation/PowerLawVertexGenerator.hpp
@@ -8,6 +8,14 @@
 
 #include "TrackingPipeline/Simulation/IVertexGenerator.hpp"
 
+inline double powerLaw(double x) {
+  const double scale = 5.31456e3;
+  const double shift = -6.98928e1;
+  const double pedestal = -4.99682e3;
+
+  return (scale * std::pow(x + shift, -0.01) + pedestal) / 20011.3;
+}
+
 namespace E320Sim {
 
 /// @brief Class that samples vertex from a ROOT histogram
@@ -23,24 +31,44 @@ struct E320PowerLawVertexGenerator : public IVertexGenerator {
   std::vector<double> zProbs;
   std::vector<double> zPositions;
 
+  const double y0 = 90.3 - 29.94176 / 2;
+  const double y1 = 331.1 + 29.94176 / 2;
+
+  std::vector<double> is{y0, y1};
+  std::vector<double> ws{powerLaw(y0), powerLaw(y1)};
+    const double a = (powerLaw(y1) - powerLaw(y0)) / (y1 - y0);
+    const double b = powerLaw(y0) - a * y0 + 1e-4;
+
   Acts::Vector3 genVertex(RandomEngine& rng) const override {
-    if (zProbs.size() == 0) {
+    if (zProbs.empty() == 0) {
       throw std::runtime_error("Vertex generator not initialized");
     }
 
     std::discrete_distribution<> discrete(zProbs.begin(), zProbs.end());
     std::uniform_real_distribution<> uniform(0, 1);
 
+    std::piecewise_linear_distribution<> linear(is.begin(), is.end(),
+                                                ws.begin());
+
     // Generate unfiorm x
     double x = xBoundLow + (xBoundHigh - xBoundLow) * uniform(rng);
 
     // Generate power-law y
-    double gamma = uniform(rng);
+    bool sampled = false;
+    double y;
+    while (!sampled) {
+      // Sample envelope
+      double envY = linear(rng);
 
-    double yTerm1 = gamma * std::pow(yBoundHigh - yShift, yPower + 1);
-    double yTerm2 = (1 - gamma) * std::pow(yBoundLow - yShift, yPower + 1);
+      // Sample ratio
+      double ratio = uniform(rng);
 
-    double y = yShift + std::pow(yTerm1 + yTerm2, 1. / (yPower + 1));
+      // Check ratio
+      if (ratio < powerLaw(envY) / (a * envY + b)) {
+        y = envY;
+        sampled = true;
+      }
+    }
 
     // Generate uniform z
     double z = zPositions.at(discrete(rng));
