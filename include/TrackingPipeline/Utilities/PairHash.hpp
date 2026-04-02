549633d4cc3189bd3fb648f64baa0aa5bcf6d7db
ssdetlab <ssetlab@gmail.com>
2026-04-02 22:37:36 +0200

    pair hash tidy

--- include/TrackingPipeline/Utilities/PairHash.hpp
+++ include/TrackingPipeline/Utilities/PairHash.hpp
@@ -6,12 +6,12 @@
 
 struct PairHash {
   std::size_t operator()(const std::pair<int, int> &t) const noexcept {
-    return std::hash<long long>()(((long long)t.first << 32) ^
-                                  (long long)t.second);
+    return std::hash<long long>()((static_cast<long long>(t.first) << 32) ^
+                                  static_cast<long long>(t.second));
   }
 
   std::size_t operator()(const std::pair<double, double> &t) const noexcept {
-    return std::hash<long long>()(((long long)t.first << 32) ^
-                                  (long long)t.second);
+    return std::hash<long long>()((static_cast<long long>(t.first) << 32) ^
+                                  static_cast<long long>(t.second));
   }
 };
