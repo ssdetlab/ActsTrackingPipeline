d647dc146781a183c6ac8f8d41e6053da0889166
ssdetlab <ssetlab@gmail.com>
2026-04-14 13:09:54 +0200

    pair hash overload

--- include/TrackingPipeline/Utilities/TupleHash.hpp
+++ include/TrackingPipeline/Utilities/TupleHash.hpp
@@ -8,6 +8,12 @@
 namespace detail {
 
 struct PairHash {
+  std::size_t operator()(
+      const std::pair<std::uint32_t, std::uint32_t> &t) const noexcept {
+    return std::hash<long long>()((static_cast<long long>(t.first) << 32) ^
+                                  static_cast<long long>(t.second));
+  }
+
   std::size_t operator()(const std::pair<int, int> &t) const noexcept {
     return std::hash<long long>()((static_cast<long long>(t.first) << 32) ^
                                   static_cast<long long>(t.second));
