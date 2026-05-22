63d22b1ab511004780c6847ea22afaa65f5f5f00
ssdetlab <ssetlab@gmail.com>
2026-05-22 06:47:13 +0200

    mag field writer

---
+++ include/TrackingPipeline/Io/E320MagneticFieldWriter.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <Acts/Utilities/Logger.hpp>
+
+#include <memory>
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TrackingPipeline/Infrastructure/IWriter.hpp"
+
+namespace E320 {
+
+class E320MagneticFieldWriter : public IWriter {
+ public:
+  /// @brief The nested configuration struct
+  struct Config {
+    /// Name of the output tree
+    std::string treeName;
+    /// The names of the output file
+    std::string filePath;
+  };
+
+  E320MagneticFieldWriter(const E320MagneticFieldWriter &) = delete;
+  E320MagneticFieldWriter(const E320MagneticFieldWriter &&) = delete;
+
+  /// @brief constructor
+  E320MagneticFieldWriter(const Config &config, Acts::Logging::Level level);
+
+  /// @brief finalize method
+  ProcessCode finalize() override;
+
+  /// @brief get the writer name
+  std::string name() const override { return "E320MagneticFieldWriter"; }
+
+  /// @brief write out data to the file
+  ProcessCode write(const AlgorithmContext &ctx) override;
+
+ protected:
+  /// @brief access to the logging instance
+  const Acts::Logger &logger() const { return *m_logger; }
+
+  /// Readonly access to the config
+  const Config &config() const { return m_cfg; }
+
+ private:
+  /// The config class
+  Config m_cfg;
+
+  /// The output file and tree
+  TFile *m_file = nullptr;
+  TTree *m_tree = nullptr;
+
+  /// Logging instance
+  std::unique_ptr<const Acts::Logger> m_logger;
+
+ protected:
+  /// Quad gradients T/m
+  double m_quad1Grad = 0;
+  double m_quad2Grad = 0;
+  double m_quad3Grad = 0;
+
+  /// XCOR strength T
+  double m_xCorrectorStrength = 0;
+
+  /// Dipole strength T
+  double m_dipoleStrength = 0;
+
+  /// Event number handle
+  ULong64_t m_eventId = 0;
+
+  /// Mutex to protect the tree filling
+  std::mutex m_mutex;
+};
+
+}  // namespace E320
---
+++ src/Io/E320MagneticFieldWriter.cpp
@@ -0,0 +1,119 @@
+#include "TrackingPipeline/Io/E320MagneticFieldWriter.hpp"
+
+#include "Acts/Definitions/Units.hpp"
+
+#include <memory>
+
+#include "TrackingPipeline/Geometry/E320GeometryConstraints.hpp"
+#include "TrackingPipeline/MagneticField/ConstantMagField.hpp"
+#include "TrackingPipeline/MagneticField/IdealQuadrupoleMagField.hpp"
+
+namespace E320 {
+
+E320MagneticFieldWriter::E320MagneticFieldWriter(const Config& config,
+                                                 Acts::Logging::Level level)
+    : m_cfg(config) {
+  if (m_cfg.filePath.empty()) {
+    throw std::invalid_argument("Missing filename");
+  }
+  if (m_cfg.treeName.empty()) {
+    throw std::invalid_argument("Missing tree name");
+  }
+
+  m_file = new TFile(m_cfg.filePath.c_str(), "RECREATE");
+  m_tree = new TTree(m_cfg.treeName.c_str(), m_cfg.treeName.c_str());
+
+  //------------------------------------------------------------------
+  // Tree branches
+  int bufSize = 32000;
+  int splitLvl = 0;
+
+  // Quad gradients T/m
+  m_tree->Branch("quad0Grad", &m_quad1Grad, bufSize, splitLvl);
+  m_tree->Branch("quad1Grad", &m_quad2Grad, bufSize, splitLvl);
+  m_tree->Branch("quad2Grad", &m_quad3Grad, bufSize, splitLvl);
+
+  // XCOR strength T
+  m_tree->Branch("xCorrectorStrength", &m_xCorrectorStrength, bufSize,
+                 splitLvl);
+
+  // Dipole strength T
+  m_tree->Branch("dipoleStrength", &m_dipoleStrength, bufSize, splitLvl);
+
+  // Event number handle
+  m_tree->Branch("eventId", &m_eventId, bufSize, splitLvl);
+}
+
+ProcessCode E320MagneticFieldWriter::finalize() {
+  if (m_file != nullptr) {
+    m_file->Write();
+    m_file->Close();
+  }
+  return ProcessCode::SUCCESS;
+}
+
+ProcessCode E320MagneticFieldWriter::write(const AlgorithmContext& ctx) {
+  std::lock_guard<std::mutex> lock(m_mutex);
+
+  m_eventId = ctx.eventNumber;
+
+  const auto& mctx = ctx.magFieldContext;
+  const auto& goInst = *E320::GeometryOptions::instance();
+
+  // Quad gradients T/m
+  m_quad1Grad =
+      goInst.quad1Gradient * Acts::UnitConstants::m / Acts::UnitConstants::T;
+  m_quad2Grad =
+      goInst.quad2Gradient * Acts::UnitConstants::m / Acts::UnitConstants::T;
+  m_quad3Grad =
+      goInst.quad3Gradient * Acts::UnitConstants::m / Acts::UnitConstants::T;
+
+  // XCOR strength T
+  m_xCorrectorStrength =
+      goInst.xCorrectorFieldStrength / Acts::UnitConstants::T;
+
+  // Dipole strength T
+  m_dipoleStrength = goInst.dipoleFieldStrength / Acts::UnitConstants::T;
+
+  if (mctx.hasValue()) {
+    const auto& store = mctx.get<std::shared_ptr<MagneticFieldStore>&>();
+
+    if (store->store.contains(goInst.quad1Id)) {
+      m_quad1Grad = store->store.at(goInst.quad1Id)
+                        .as<IdealQuadrupoleMagField::Cache>()
+                        .m_gradient *
+                    Acts::UnitConstants::m / Acts::UnitConstants::T;
+    }
+    if (store->store.contains(goInst.quad2Id)) {
+      m_quad2Grad = store->store.at(goInst.quad2Id)
+                        .as<IdealQuadrupoleMagField::Cache>()
+                        .m_gradient *
+                    Acts::UnitConstants::m / Acts::UnitConstants::T;
+    }
+    if (store->store.contains(goInst.quad3Id)) {
+      m_quad3Grad = store->store.at(goInst.quad3Id)
+                        .as<IdealQuadrupoleMagField::Cache>()
+                        .m_gradient *
+                    Acts::UnitConstants::m / Acts::UnitConstants::T;
+    }
+    if (store->store.contains(goInst.xCorrectorId)) {
+      m_xCorrectorStrength = store->store.at(goInst.xCorrectorId)
+                                 .as<ConstantMagField::Cache>()
+                                 .m_field(goInst.xCorrectorDirIdx) /
+                             Acts::UnitConstants::T;
+    }
+    if (store->store.contains(goInst.dipoleId)) {
+      m_dipoleStrength = store->store.at(goInst.dipoleId)
+                             .as<ConstantMagField::Cache>()
+                             .m_field(goInst.dipoleDirIdx) /
+                         Acts::UnitConstants::T;
+    }
+  }
+
+  m_tree->Fill();
+
+  // Return success flag
+  return ProcessCode::SUCCESS;
+}
+
+}  // namespace E320
