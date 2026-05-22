56ac04d33543794980148d096bb1552a4e978d95
ssdetlab <ssetlab@gmail.com>
2026-05-22 06:46:50 +0200

    magnetic field provider

---
+++ include/TrackingPipeline/Io/E320MagneticFieldParametersProvider.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <memory>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TrackingPipeline/MagneticField/MagneticFieldStoreCollection.hpp"
+
+namespace E320 {
+
+class E320MagneticFieldParametersProvider {
+ public:
+  /// @brief The nested configuration struct
+  struct Config {
+    /// Name of the input tree
+    std::string treeName;
+    /// The names of the input files
+    std::vector<std::string> filePaths;
+  };
+
+  /// @brief Constructor
+  ///
+  /// @param config The Configuration struct
+  explicit E320MagneticFieldParametersProvider(const Config& config);
+
+  /// Get field store collection
+  std::shared_ptr<MagneticFieldStoreCollection>
+  getMagneticFieldStoreCollection();
+
+  /// Readonly access to the config
+  const Config& config() const { return m_cfg; }
+
+ private:
+  /// The config class
+  Config m_cfg;
+
+  /// The input file and tree
+  TFile* m_file = nullptr;
+  TTree* m_tree = nullptr;
+  TChain* m_chainOwner = nullptr;
+
+ protected:
+  /// Alignment store
+  std::shared_ptr<MagneticFieldStoreCollection> m_storeCollection = nullptr;
+
+  /// Quad gradients T/m
+  double m_quad1Grad = 0;
+  double m_quad2Grad = 0;
+  double m_quad3Grad = 0;
+
+  /// XCOR strength T
+  double m_xCorrectorStrength = 0;
+
+  /// Dipole strength T
+  double m_dipoleStrength = 0;
+
+  /// Event number handle
+  ULong64_t m_eventId = 0;
+};
+
+}  // namespace E320
---
+++ src/Io/E320MagneticFieldParametersProvider.cpp
@@ -0,0 +1,108 @@
+#include "TrackingPipeline/Io/E320MagneticFieldParametersProvider.hpp"
+
+#include "Acts/Definitions/Units.hpp"
+
+#include <cstddef>
+#include <memory>
+
+#include "TrackingPipeline/Geometry/E320GeometryConstraints.hpp"
+#include "TrackingPipeline/MagneticField/ConstantMagField.hpp"
+#include "TrackingPipeline/MagneticField/IdealQuadrupoleMagField.hpp"
+
+namespace E320 {
+
+E320MagneticFieldParametersProvider::E320MagneticFieldParametersProvider(
+    const Config& config)
+    : m_cfg(config) {
+  if (m_cfg.filePaths.empty()) {
+    throw std::invalid_argument("Missing input filenames");
+  }
+  if (m_cfg.treeName.empty()) {
+    throw std::invalid_argument("Missing tree name");
+  }
+
+  if (m_cfg.filePaths.size() == 1) {
+    m_file = new TFile(m_cfg.filePaths.at(0).c_str());
+    m_tree = m_file->Get<TTree>(m_cfg.treeName.c_str());
+  } else {
+    m_chainOwner = new TChain(m_cfg.treeName.c_str());
+    // Add the files to the chain
+    for (const auto& path : m_cfg.filePaths) {
+      m_chainOwner->Add(path.c_str());
+    }
+    m_tree = dynamic_cast<TTree*>(m_chainOwner);
+  }
+
+  // Quad gradients T/m
+  m_tree->SetBranchAddress("quad0Grad", &m_quad1Grad);
+  m_tree->SetBranchAddress("quad1Grad", &m_quad2Grad);
+  m_tree->SetBranchAddress("quad2Grad", &m_quad3Grad);
+
+  // XCOR strength T
+  m_tree->SetBranchAddress("xCorrectorStrength", &m_xCorrectorStrength);
+
+  // Dipole strength T
+  m_tree->SetBranchAddress("dipoleStrength", &m_dipoleStrength);
+
+  // Event number handle
+  m_tree->SetBranchAddress("eventId", &m_eventId);
+
+  // Geometry constraints instance
+  const auto& goInst = *GeometryOptions::instance();
+
+  std::size_t longIdx = goInst.longIdx;
+  std::size_t shortIdx = goInst.shortIdx;
+
+  std::size_t quad1Id = goInst.quad1Id;
+  std::size_t quad2Id = goInst.quad2Id;
+  std::size_t quad3Id = goInst.quad3Id;
+
+  std::size_t nEntries = m_tree->GetEntries();
+  MagneticFieldStores stores;
+  stores.reserve(nEntries);
+  for (std::size_t i = 0; i < nEntries; i++) {
+    m_tree->GetEntry(i);
+
+    auto store = std::make_shared<MagneticFieldStore>();
+    store->store.reserve(5);
+
+    store->store.insert(
+        {goInst.quad1Id,
+         Acts::MagneticFieldProvider::Cache(
+             std::in_place_type<IdealQuadrupoleMagField::Cache>,
+             m_quad1Grad * Acts::UnitConstants::T / Acts::UnitConstants::m)});
+    store->store.insert(
+        {goInst.quad2Id,
+         Acts::MagneticFieldProvider::Cache(
+             std::in_place_type<IdealQuadrupoleMagField::Cache>,
+             m_quad2Grad * Acts::UnitConstants::T / Acts::UnitConstants::m)});
+    store->store.insert(
+        {goInst.quad3Id,
+         Acts::MagneticFieldProvider::Cache(
+             std::in_place_type<IdealQuadrupoleMagField::Cache>,
+             m_quad3Grad * Acts::UnitConstants::T / Acts::UnitConstants::m)});
+
+    store->store.insert(
+        {goInst.dipoleId,
+         Acts::MagneticFieldProvider::Cache(
+             std::in_place_type<ConstantMagField::Cache>,
+             m_dipoleStrength * Acts::UnitConstants::T, shortIdx)});
+
+    store->store.insert(
+        {goInst.xCorrectorId,
+         Acts::MagneticFieldProvider::Cache(
+             std::in_place_type<ConstantMagField::Cache>,
+             m_xCorrectorStrength * Acts::UnitConstants::T, longIdx)});
+
+    stores.insert({m_eventId, std::move(store)});
+  }
+
+  m_storeCollection = std::make_shared<MagneticFieldStoreCollection>(stores);
+}
+
+std::shared_ptr<MagneticFieldStoreCollection>
+E320MagneticFieldParametersProvider::getMagneticFieldStoreCollection() {
+  return m_storeCollection;
+}
+
+}  // namespace E320
