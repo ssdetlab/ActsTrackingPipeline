7b5a342c7cf2152e449849efcde565c9cd31942f
ssdetlab <ssetlab@gmail.com>
2026-07-23 22:55:12 +0200

    extended source link docs, phi-correcting calibrator

--- include/TrackingPipeline/EventData/ExtendedSourceLink.hpp
+++ include/TrackingPipeline/EventData/ExtendedSourceLink.hpp
@@ -15,12 +15,15 @@
 #include <cassert>
 #include <cstddef>
 
-/// @brief Extended source link implementation
+/// @brief Extended source link holding the particle hits
+/// and the angles in the track coordinate system
 class ExtendedSourceLink {
  public:
-  static constexpr std::size_t localSubspaceSize = 5;
-  static constexpr std::size_t globalSubspaceSize = 7;
+  /// Subspace sizes
+  static constexpr const std::size_t localSubspaceSize = 4;
+  static constexpr const std::size_t globalSubspaceSize = 6;
 
+  /// @brief Surface accessor struct
   struct SurfaceAccessor {
     const Acts::Experimental::Detector* detector = nullptr;
 
@@ -30,25 +33,36 @@ class ExtendedSourceLink {
     }
   };
 
-  /// Construct a 5d source link
+  /// @brief Constructor
+  ///
+  /// @param paramsLoc particle hit and angles in the track coordinate system
+  /// @param paramsGlob particle hit and momentum direction in the global frame
+  /// @param cov covariance of the measurement in the track coordinate system
+  /// @param gid geometry ID of the measurement surface
+  /// @param eid event ID
+  /// @param idx user-assigned index for fast-sim fast access
+  /// @param backwards flag indicating reverse propagation of the track
   ExtendedSourceLink(const Acts::ActsVector<localSubspaceSize>& paramsLoc,
                      const Acts::ActsVector<globalSubspaceSize>& paramsGlob,
                      const Acts::ActsSquareMatrix<localSubspaceSize>& cov,
-                     const Acts::GeometryIdentifier& gid, int eid, int idx)
+                     const Acts::GeometryIdentifier& gid, int eid, int idx,
+                     bool backwards)
       : m_geometryId(gid),
         m_eventId(eid),
         m_index(idx),
         m_parametersLoc(paramsLoc),
         m_parametersGlob(paramsGlob),
-        m_covariance(cov) {}
+        m_covariance(cov),
+        m_backwards(backwards) {}
 
-  /// Default-construct an invalid source link to satisfy SourceLinkConcept.
+  /// Delete default-construct to satisfy SourceLinkConcept.
   ExtendedSourceLink() = delete;
   ExtendedSourceLink(const ExtendedSourceLink&) = default;
   ExtendedSourceLink(ExtendedSourceLink&&) = default;
   ExtendedSourceLink& operator=(const ExtendedSourceLink&) = default;
   ExtendedSourceLink& operator=(ExtendedSourceLink&&) = default;
 
+  /// @brief Equality operator
   bool operator==(const ExtendedSourceLink& rhs) const {
     return (m_geometryId == rhs.geometryId()) && (m_eventId == rhs.eventId()) &&
            (m_indices == rhs.indices()) &&
@@ -56,34 +70,63 @@ class ExtendedSourceLink {
            (m_covariance == rhs.covariance());
   }
 
+  /// @brief Inequality operator
   bool operator!=(const ExtendedSourceLink& rhs) const {
     return !(*this == rhs);
   }
 
+  /// @brief Get source link measurement indices
+  ///
+  /// @return array with indices indicating the measurement axes
   std::array<Acts::BoundIndices, localSubspaceSize> indices() const {
     return m_indices;
   }
 
+  /// @brief Get user-assigned index
+  ///
+  /// @return user-assigned index
   int index() const { return m_index; }
 
+  /// @brief Get event ID
+  ///
+  /// @return event ID
   int eventId() const { return m_eventId; }
 
+  /// @brief Get geometry ID
+  ///
+  /// @return geometry ID
   Acts::GeometryIdentifier geometryId() const { return m_geometryId; }
 
+  /// @brief Get source link parameters in the track frame
+  ///
+  /// @return source link parameters in the track frame
   Acts::ActsVector<localSubspaceSize> parametersLoc() const {
     return m_parametersLoc;
   }
 
+  /// @brief Get source link parameters in the global frame
+  ///
+  /// @return source link parameters in the global frame
   Acts::ActsVector<globalSubspaceSize> parametersGlob() const {
     return m_parametersGlob;
   }
 
+  /// @brief Get source link covariance in the track frame
+  ///
+  /// @return source link covariance in the track frame
   Acts::ActsSquareMatrix<localSubspaceSize> covariance() const {
     return m_covariance;
   }
 
+  /// @brief Get source link bacwards propagation flag
+  ///
+  /// @return source link bacwards propagation flag
+  bool isBackwards() const { return m_backwards; }
+
+  /// @brief Set source link user-assigned index
   void setIndex(int idx) { m_index = idx; }
 
+  /// @brief Set source link event ID
   void setEventId(int eid) { m_eventId = eid; }
 
  private:
@@ -98,8 +141,7 @@ class ExtendedSourceLink {
 
   /// Indices of the local coordinates
   std::array<Acts::BoundIndices, localSubspaceSize> m_indices = {
-      Acts::eBoundLoc0, Acts::eBoundLoc1, Acts::eBoundPhi, Acts::eBoundTheta,
-      Acts::eBoundQOverP};
+      Acts::eBoundLoc0, Acts::eBoundLoc1, Acts::eBoundPhi, Acts::eBoundTheta};
 
   /// Local hit coordinates
   Acts::ActsVector<localSubspaceSize> m_parametersLoc;
@@ -109,12 +151,17 @@ class ExtendedSourceLink {
 
   /// Covariance matrix
   Acts::ActsSquareMatrix<localSubspaceSize> m_covariance;
+
+  /// Backwards propagation flag
+  bool m_backwards;
 };
 
-/// Extract the measurement from a ExtendedSourceLink.
+/// Extract the measurement from an ExtendedSourceLink.
 ///
-/// @param gctx Unused
-/// @param trackState TrackState to calibrated
+/// @param gctx unused
+/// @param cctx unused
+/// @param sourceLink source link with the measurement
+/// @param trackState TrackState to be calibrated
 template <typename trajectory_t>
 void extendedSourceLinkCalibratorReturn(
     const Acts::GeometryContext& /*gctx*/,
@@ -135,20 +182,88 @@ void extendedSourceLinkCalibratorReturn(
   trackState.setProjector(
       Acts::detail::FixedSizeSubspace<Acts::BoundIndices::eBoundSize,
                                       ExtendedSourceLink::localSubspaceSize>(
-          std::array{indices[0], indices[1], indices[2], indices[3],
-                     indices[4]})
+          std::array{
+              indices[0],
+              indices[1],
+              indices[2],
+              indices[3],
+          })
           .projector<double>());
 }
 
-/// Extract the measurement from a ExtendedSourceLink.
+/// Extract the measurement from an ExtendedSourceLink.
 ///
-/// @param gctx Unused
+/// @param gctx unused
+/// @param cctx unused
+/// @param sourceLink source link with the measurement
 /// @param trackState TrackState to calibrated
 template <typename trajectory_t>
 void extendedSourceLinkCalibrator(
     const Acts::GeometryContext& gctx, const Acts::CalibrationContext& cctx,
     const Acts::SourceLink& sourceLink,
     typename trajectory_t::TrackStateProxy trackState) {
+  // std::cout << "EXTENDED SL CALIBRATOR CALL\n";
   extendedSourceLinkCalibratorReturn<trajectory_t>(gctx, cctx, sourceLink,
                                                    trackState);
+}
+
+/// Extract the measurement from an ExtendedSourceLink.
+///
+/// @param gctx unused
+/// @param cctx unused
+/// @param sourceLink source link with the measurement
+/// @param trackState TrackState to be calibrated
+template <typename trajectory_t>
+void extendedSourceLinkBackwardsPhiCorrectionCalibratorReturn(
+    const Acts::GeometryContext& /*gctx*/,
+    const Acts::CalibrationContext& /*cctx*/,
+    const Acts::SourceLink& sourceLink,
+    typename trajectory_t::TrackStateProxy trackState) {
+  const auto& sl = sourceLink.template get<ExtendedSourceLink>();
+  Acts::ActsVector<ExtendedSourceLink::localSubspaceSize> loc =
+      sl.parametersLoc();
+  if (sl.isBackwards()) {
+    double currentDiff =
+        loc(Acts::eBoundPhi) - trackState.predicted()(Acts::eBoundPhi);
+    if (currentDiff < 0 && std::abs(currentDiff) > M_PI) {
+      loc(Acts::eBoundPhi) += 2 * M_PI;
+    }
+    if (currentDiff > M_PI) {
+      loc(Acts::eBoundPhi) -= 2 * M_PI;
+    }
+  }
+
+  trackState.setUncalibratedSourceLink(sourceLink);
+
+  trackState.allocateCalibrated(ExtendedSourceLink::localSubspaceSize);
+  trackState.template calibrated<ExtendedSourceLink::localSubspaceSize>() = loc;
+  trackState
+      .template calibratedCovariance<ExtendedSourceLink::localSubspaceSize>() =
+      sl.covariance();
+  const auto& indices = sl.indices();
+  trackState.setProjector(
+      Acts::detail::FixedSizeSubspace<Acts::BoundIndices::eBoundSize,
+                                      ExtendedSourceLink::localSubspaceSize>(
+          std::array{
+              indices[0],
+              indices[1],
+              indices[2],
+              indices[3],
+          })
+          .projector<double>());
+}
+
+/// Extract the measurement from an ExtendedSourceLink.
+///
+/// @param gctx unused
+/// @param cctx unused
+/// @param sourceLink source link with the measurement
+/// @param trackState TrackState to calibrated
+template <typename trajectory_t>
+void extendedSourceLinkBackwardsPhiCorrectionCalibrator(
+    const Acts::GeometryContext& gctx, const Acts::CalibrationContext& cctx,
+    const Acts::SourceLink& sourceLink,
+    typename trajectory_t::TrackStateProxy trackState) {
+  extendedSourceLinkBackwardsPhiCorrectionCalibratorReturn<trajectory_t>(
+      gctx, cctx, sourceLink, trackState);
 }
