0f60dbf2883a7ad1b7b54fffe17d14e8e6dc03d1
ssdetlab <ssetlab@gmail.com>
2026-08-06 14:57:52 +0200

    e320 track estimator headers cleanup

--- src/TrackFinding/E320TrackParametersEstimator.cpp
+++ src/TrackFinding/E320TrackParametersEstimator.cpp
@@ -2,7 +2,6 @@
 
 #include "Acts/Definitions/Algebra.hpp"
 #include "Acts/Definitions/TrackParametrization.hpp"
-#include "Acts/EventData/SourceLink.hpp"
 #include "Acts/EventData/TrackParameters.hpp"
 #include "Acts/MagneticField/MagneticFieldContext.hpp"
 #include "Acts/Utilities/Result.hpp"
